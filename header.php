4f3bc05bd8cea6cabd7d8c4906aaa99a4dc749ab
Alan Rodrigues <alancrato@hotmail.com>
2018-08-27 17:15:46 -0300

    inciando novo layout do cn7

---
+++ header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+    <title>Ceará News 7 - Sete dias da semana sem medo da notícia!</title>
+
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+    <link href="img/favicon.png" rel="icon"/>
+</head>
+<body>
+<div class="top">
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m6">
+                Sete dias da semana sem medo da notícia
+            </div>
+            <div class="col s12 m6">
+                <div class="right">
+                    f i w t
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<nav class="black" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="#">Destaques</a></li>
+            <li><a href="#">Editorias</a></li>
+            <li><a href="#">Últimas</a></li>
+            <li><a href="#">Mais Lidas</a></li>
+            <li><a href="#">Contato</a></li>
+            <li><a href="#">Jornal do Cariri</a></li>
+            <li><a href="#">Plus Fm</a></li>
+        </ul>
+
+        <ul id="nav-mobile" class="sidenav">
+            <li><a href="#">Navbar Link</a></li>
+        </ul>
+        <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,281 @@
+<?php
+    require 'header.php';
+?>
+
+  <div class="row"></div>
+
+  <div class="container">
+      <div class="row">
+          <div class="col s12 m12 l12">
+              <div class="pub-top"></div>
+          </div>
+      </div>
+  </div>
+
+  <div class="container">
+      <div class="row">
+          <div class="col s12 m5 l5">
+              <div class="btn-small">
+                  Crime Organizado
+              </div>
+              <div class="title">
+                  Mortes de bandidos do PCC e do GDE provocam "salve geral" para ataques na Capital
+              </div>
+              <div class="bigode">
+                  Criminosos foram mortos na manha de quinta deira passada durante operação da Polícia Cicil, em Amontada. Em represália, as facções ordenadas a seus "soldados" iniciar uma nova onda de ataques nas ruas das cidades
+              </div>
+              <div class="line"></div>
+          </div>
+          <div class="col s12 m7 l7">
+              <div class="row">
+                  <div class="col s12 m5 l5">
+                      <img src="img/cards/nezinho.jpg"/>
+                      <div class="btn-small">
+                          Dinheiro Muito
+                      </div>
+                      <div class="title-destaque">
+                          Uma trajetória impressionante: de zero reais em 1988 para R$ 2 milhões em 2018
+                      </div>
+                      <div class="bigode-destaque">
+                          Nezinho fica rico na Prefeitura de Horizonte e compra mansão por 1 milhão de dólares
+                      </div>
+                      <div class="line"></div>
+                  </div>
+                  <div class="col s12 m7 l7">
+                      <div class="row">
+                          <div class="col s12 m12 l12">
+                              <div class="row">
+                                    <div class="col s4 m4 l4">
+                                        <img src="img/cards/ciro2.jpg"/>
+                                    </div>
+                                    <div class="col s8 m8 l8">
+                                        <div class="retranca-destaque-right">
+                                            Fato ou Fake?
+                                        </div>
+                                        <div class="tittle-destaque-right">
+                                            Ciro Gomes vende imagem de machão e afirma não andar com seguranças em Fortaleza
+                                        </div>
+                                    </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="line"></div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      <div class="row">
+          <div class="col s12 m5 l5">
+              <div class="float--mais-destaques">
+                  <div class="row">
+                      <div class="col s6 l6">
+                          <div class="mais-destaque">
+                              Mais Destaques
+                          </div>
+                      </div>
+                      <div class="col s6 l6">
+                          <div class="border-mais-destaques"></div>
+                      </div>
+                  </div>
+                  <div class="row">
+                      <div class="card">
+                          <div class="card-image">
+                              <img src="img/cards/ciro.jpg"/>
+                              <div class="card-container">
+                                  <div class="row">
+                                      <div class="btn-small bigode-mais-destaques">
+                                          Ciro Gomes
+                                      </div>
+                                  </div>
+                                  <div class="card-title">
+                                      We are a team of college students working on this project like it's our full time job
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="row">
+                      <div class="card">
+                          <div class="card-image">
+                              <img src="img/cards/ciro.jpg"/>
+                              <div class="card-container">
+                                  <div class="row">
+                                      <div class="btn-small bigode-mais-destaques">
+                                          Ciro Gomes
+                                      </div>
+                                  </div>
+                                  <div class="card-title">
+                                      We are a team of college students working on this project like it's our full time job
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="row">
+                      <div class="card">
+                          <div class="card-image">
+                              <img src="img/cards/ciro.jpg"/>
+                              <div class="card-container">
+                                  <div class="row">
+                                      <div class="btn-small bigode-mais-destaques">
+                                          Ciro Gomes
+                                      </div>
+                                  </div>
+                                  <div class="card-title">
+                                      We are a team of college students working on this project like it's our full time job
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="line"></div>
+          </div>
+          <div class="col s12 m7 l7">
+              <div class="row">
+                  <div class="col s12 m6 l6">
+                      <div class="retranca-destaque-right">
+                          Fato ou Fake?
+                      </div>
+                      <div class="tittle-destaque-right">
+                          Ciro Gomes vende imagem de machão e afirma não andar com seguranças em Fortaleza
+                      </div>
+                      <br/>
+
+                      <div class="retranca-destaque-right">
+                          Fato ou Fake?
+                      </div>
+                      <div class="tittle-destaque-right">
+                          Ciro Gomes vende imagem de machão e afirma não andar com seguranças em Fortaleza
+                      </div>
+                      <br/>
+
+                      <div class="retranca-destaque-right">
+                          Fato ou Fake?
+                      </div>
+                      <div class="tittle-destaque-right">
+                          Ciro Gomes vende imagem de machão e afirma não andar com seguranças em Fortaleza
+                      </div>
+                      <br/>
+
+                      <div class="row brasilia">
+                          <div class="col s6 l6">
+                              <div class="mais-destaque">
+                                  Conexão Brasília
+                              </div>
+                          </div>
+                          <div class="col s6 l6">
+                              <div class="border-mais-destaques"></div>
+                          </div>
+                          <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/179576144&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">
+                          </iframe>
+                      </div>
+                      <div class="line"></div>
+
+                  </div>
+
+                  <div class="col s12 m6 l6">
+                      <div class="retranca-destaque-right">
+                          Publicidade
+                      </div>
+                      <div class="publicidade-quadrada"></div>
+                      <br/>
+
+                      <div class="col s6 l6">
+                          <div class="mais-destaque">
+                              Plantão Plus
+                          </div>
+                      </div>
+                      <div class="col s6 l6">
+                          <div class="border-mais-destaques"></div>
+                      </div>
+                  </div>
+
+              </div>
+          </div>
+      </div>
+  </div>
+
+  <div class="row"></div>
+
+  <div class="container">
+      <div class="row">
+          <div class="col s12 m12 l12">
+              <div class="pub-center"></div>
+          </div>
+      </div>
+  </div>
+
+  <div class="row"></div>
+
+  <div class="container">
+      <div class="row colunas">
+          <div class="col s12 m2 l2 center">
+              <img src="img/colunas/home_01@2x.png"/>
+              <div class="title-coluna">
+                  Argemiro e Fernando sob bandeira branca
+              </div>
+              <div class="btn-small">
+                  Donizete Arruda
+              </div>
+          </div>
+          <div class="col s12 m2 l2 center">
+              <img src="img/colunas/home_02@2x.png"/>
+              <div class="title-coluna">
+                  Ajuda do exército seria bem vinda
+              </div>
+              <div class="btn-small">
+                  Fernando Ribeiro
+              </div>
+          </div>
+          <div class="col s12 m2 l2 center">
+              <img src="img/colunas/home_03@2x.png"/>
+              <div class="title-coluna">
+                  Ouça o Quadro palavra de Fé
+              </div>
+              <div class="btn-small">
+                  Pastor Jecer Goes
+              </div>
+          </div>
+          <div class="col s12 m2 l2 center">
+              <img src="img/colunas/home_04@2x.png"/>
+              <div class="title-coluna">
+                  Economia ou Gastança?
+              </div>
+              <div class="btn-small">
+                  Madson Vagner
+              </div>
+          </div>
+          <div class="col s12 m2 l2 center">
+              <img src="img/colunas/home_05@2x.png"/>
+              <div class="title-coluna">
+                  Arquitetura dos Sonhos
+              </div>
+              <div class="btn-small">
+                  Raphael Barros
+              </div>
+          </div>
+          <div class="col s12 m2 l2 center">
+              <img src="img/colunas/home_06@2x.png"/>
+              <div class="title-coluna">
+                  Pressão
+              </div>
+              <div class="btn-small">
+                  Roberto Pires
+              </div>
+          </div>
+      </div>
+  </div>
+
+<div class="row"></div>
+
+<div class="container">
+    <div class="row">
+        line
+    </div>
+</div>
+
+<?php
+  require 'footer.php';
+?>
