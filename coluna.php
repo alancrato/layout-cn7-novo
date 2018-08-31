3bb3b185e9d1e640aff06834c6ec53fbe08990d4
Alan Rodrigues <alancrato@hotmail.com>
2018-08-31 10:07:02 -0300

    coluna

---
+++ coluna.php
@@ -0,0 +1,163 @@
+<?php
+    require 'header.php';
+?>
+
+<br/>
+<div class="container">
+
+    <div class="row hide-on-med-and-down">
+        <div class="col s12 m1 l1"></div>
+        <div class="col s12 m10 l10">
+            <div class="pub-top"></div>
+        </div>
+        <div class="col s12 m1 l1"></div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+
+        <div class="col s12 m2 l2 center">
+            <div class="ajuste-colunista">
+                <img src="img/colunas/home_01@2x.png"/>
+                <div class="btn-small ajuste-title-colunista">
+                    Donizete Arruda
+                </div>
+                <div class="title-coluna">
+                    Jornalista
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m7 l7">
+            <div class="title-single ajuste-padding-coluna">
+                Argemiro e Fernando sobre bandeira branca
+            </div>
+
+            <div class="content-single-coluna">
+                juiz de Direito da Comarca de Juazeiro do Norte, Marcelo Wolney Alencar Pereira de Matos, determinou, no dia 16, que a empresa SOCIL – Sociedade de Comércio e Imóveis LTDA, por meio de seus sócios Virgínia Maria Bezerra Mendonça de Alencar, Henrique Luiz Bezerra de Mendonça, Roberta Bezerra de Mendonça Pinheiro e José Adail de Mendonça, bem como o município de Juazeiro do Norte apresentem em juízo todos os contratos celebrados com os adquirentes dos lotes situados no loteamento “Campo Alegre”. O magistrado também decretou, a indisponibilidade dos bens da SOCIL, com base no poder geral de cautela, a fim de assegurar o resultado útil do processo.
+
+                <p></p>
+                juiz de Direito da Comarca de Juazeiro do Norte, Marcelo Wolney Alencar Pereira de Matos, determinou, no dia 16, que a empresa SOCIL – Sociedade de Comércio e Imóveis LTDA, por meio de seus sócios Virgínia Maria Bezerra Mendonça de Alencar, Henrique Luiz Bezerra de Mendonça, Roberta Bezerra de Mendonça Pinheiro e José Adail de Mendonça, bem como o município de Juazeiro do Norte apresentem em juízo todos os contratos celebrados com os adquirentes dos lotes situados no loteamento “Campo Alegre”. O magistrado também decretou, a indisponibilidade dos bens da SOCIL, com base no poder geral de cautela, a fim de assegurar o resultado útil do processo.
+            </div>
+
+            <br/>
+
+            <div class="row">
+                <div class="col s12 m3 l3">
+                </div>
+                <div class="col s6 m3 l3">
+                    <div class="time-date img-date-coluna">
+                        <img src="img/social/single/time.png"/>
+                    </div>
+                    <div class="date-single">
+                        <?= date('d/m H:i'); ?>
+                    </div>
+                </div>
+                <div class="col s6 m3 l3">
+                    <div class="row right ajuste-img-coluna ajuste-img-social-mobile">
+                        <div class="col s4 facebook-coluna">
+                            <img src="img/social/single/facebook.png"/>
+                        </div>
+                        <div class="col s4">
+                            <img src="img/social/single/whatsapp.png"/>
+                        </div>
+                        <div class="col s4">
+                            <img src="img/social/single/twitter.png"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m3 l3">
+                </div>
+            </div>
+
+            <div class="line-category"></div>
+            <br/>
+
+            <div class="title-single ajuste-padding-coluna">
+                Argemiro e Fernando sobre bandeira branca
+            </div>
+
+            <div class="content-single-coluna">
+                juiz de Direito da Comarca de Juazeiro do Norte, Marcelo Wolney Alencar Pereira de Matos, determinou, no dia 16, que a empresa SOCIL – Sociedade de Comércio e Imóveis LTDA, por meio de seus sócios Virgínia Maria Bezerra Mendonça de Alencar, Henrique Luiz Bezerra de Mendonça, Roberta Bezerra de Mendonça Pinheiro e José Adail de Mendonça, bem como o município de Juazeiro do Norte apresentem em juízo todos os contratos celebrados com os adquirentes dos lotes situados no loteamento “Campo Alegre”. O magistrado também decretou, a indisponibilidade dos bens da SOCIL, com base no poder geral de cautela, a fim de assegurar o resultado útil do processo.
+
+                <p></p>
+                juiz de Direito da Comarca de Juazeiro do Norte, Marcelo Wolney Alencar Pereira de Matos, determinou, no dia 16, que a empresa SOCIL – Sociedade de Comércio e Imóveis LTDA, por meio de seus sócios Virgínia Maria Bezerra Mendonça de Alencar, Henrique Luiz Bezerra de Mendonça, Roberta Bezerra de Mendonça Pinheiro e José Adail de Mendonça, bem como o município de Juazeiro do Norte apresentem em juízo todos os contratos celebrados com os adquirentes dos lotes situados no loteamento “Campo Alegre”. O magistrado também decretou, a indisponibilidade dos bens da SOCIL, com base no poder geral de cautela, a fim de assegurar o resultado útil do processo.
+            </div>
+
+            <br/>
+
+            <div class="row">
+                <div class="col s12 m3 l3">
+                </div>
+                <div class="col s6 m3 l3">
+                    <div class="time-date img-date-coluna">
+                        <img src="img/social/single/time.png"/>
+                    </div>
+                    <div class="date-single">
+                        <?= date('d/m H:i'); ?>
+                    </div>
+                </div>
+                <div class="col s6 m3 l3">
+                    <div class="row right ajuste-img-coluna ajuste-img-social-mobile">
+                        <div class="col s4 facebook-coluna">
+                            <img src="img/social/single/facebook.png"/>
+                        </div>
+                        <div class="col s4">
+                            <img src="img/social/single/whatsapp.png"/>
+                        </div>
+                        <div class="col s4">
+                            <img src="img/social/single/twitter.png"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m3 l3">
+                </div>
+            </div>
+
+            <div class="line-category"></div>
+            <br/>
+
+            <ul class="pagination center">
+                <li class="disabled"><a href="javascript:void(null)"><i class="material-icons">chevron_left</i></a></li>
+                <li class="active"><a href="javascript:void(null)">1</a></li>
+                <li class="waves-effect"><a href="javascript:void(null)">2</a></li>
+                <li class="waves-effect"><a href="javascript:void(null)">3</a></li>
+                <li class="waves-effect"><a href="javascript:void(null)">4</a></li>
+                <li class="waves-effect"><a href="javascript:void(null)">5</a></li>
+                <li class="waves-effect"><a href="javascript:void(null)"><i class="material-icons">chevron_right</i></a></li>
+            </ul>
+
+        </div>
+        <div class="col s12 m3 l3">
+            <div class="ajuste-padding-coluna">
+                <div class="title-publicidade center">
+                    Publicidade
+                </div>
+                <div class="pub-category"></div>
+                <br/>
+                <div class="line-sidebar-category"></div>
+                <br/>
+                <div class="facebook-page center hide-on-med-and-down">
+                    <div id="fb-root"></div>
+                    <script>(function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id)) return;
+                            js = d.createElement(s); js.id = id;
+                            js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v3.1&appId=1719551754798065&autoLogAppEvents=1';
+                            fjs.parentNode.insertBefore(js, fjs);
+                        }(document, 'script', 'facebook-jssdk'));
+                    </script>
+                    <div class="fb-page" data-href="https://www.facebook.com/cearanews7/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/cearanews7/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/cearanews7/">Ceará News 7</a></blockquote></div>
+                </div>
+                <br/>
+                <div class="line-sidebar-category hide-on-med-and-down"></div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<?php
+    require 'footer.php';
+?>
